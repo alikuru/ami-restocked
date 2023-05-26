2c1b35c20666834187cc56cdcfe778bb6804b7c9
Ali Kuru <ali@alikuru.com>
2023-05-27 00:04:44 +0300

    Timestamps added to log stdout.

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ const emailSettings = {
     }
 };
 
+function logDate() {
+    return new Date().toISOString();
+};
+
 // Create an email transporter
 const transporter = nodemailer.createTransport(emailSettings);
 
@@ -36,7 +40,7 @@ const sendEmailAlert = async (price, deposit, reservationStatus) => {
             subject: 'Citroen AMI restocked',
             text: `The Citroen AMI with orange color option might be on sale again, better check the ordering page ASAP 👉 https://talep.citroen.com.tr/amionlinesiparis/form/turuncu\n\nThe car is priced at ${currencyFormatter.format(price)} and asked deposit amount is ${currencyFormatter.format(deposit)}. Currently, reservations are ${reservationStatus ? 'also open' : 'not open'}.`
         });
-        console.log('Email alert sent successfully.');
+        console.log(`${logDate()} :: Email alert sent successfully.`);
     } catch (error) {
         console.error('Error sending email:', error);
     }
@@ -51,9 +55,12 @@ const fetchDataAndCheckChanges = async () => {
         const amiPrice = response.data.carPrice;
         const amiDeposit = response.data.ccAmount;
 
+        // Log each attempt to the stdout
+        console.log(`${logDate()} :: Checking the AMI availability...`);
+
         // Check if the stock status has changed
         if (amiStock == true) {
-            console.log('Stock status has been updated!');
+            console.log(`${logDate()} :: Stock status has been updated!`);
             await sendEmailAlert(amiPrice, amiDeposit, amiReservation);
         }
     } catch (error) {
